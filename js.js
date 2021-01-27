8dc9c2aaa2bfc8300b6d3a694838bd804dd248e5
Park Seonghun <seonghun120614@gmail.com>
2021-01-27 14:56:49 +0000

    completed 95%

---
+++ js.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/*function Modal() {
+    let modal_status = document.getElementById('modal').style.display;
+    if (modal_status='block') {
+        modal_status = 'none';
+        alert('off');
+    } else if (modal_status='none') {
+        modal_status = 'block';
+        alert('on');
+    }
+}*/
