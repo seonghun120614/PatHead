50d8c350c8cff935fbbdaa6b109e45a8c8c2ab2b
Park Seonghun <seonghun120614@gmail.com>
2021-01-29 10:17:36 +0000

    i will upgrade my main page.. 50% completed

--- js.js
+++ js.js
@@ -1,12 +1,22 @@
 'use strict'
 
-/*function Modal() {
-    let modal_status = document.getElementById('modal').style.display;
-    if (modal_status='block') {
-        modal_status = 'none';
-        alert('off');
-    } else if (modal_status='none') {
-        modal_status = 'block';
-        alert('on');
+let modal_status = false;
+
+function modal_switch() {
+    if (modal_status === false) {
+        modal_status = true;
+        document.getElementById('modal').style.display = 'block';
+    } else {
+        modal_status = false;
+        document.getElementById('modal').style.display = 'none';
     }
-}*/
+}
+
+window.onload = function() {
+    document.querySelector('.item').onclick = function() {
+        modal_switch();
+    }
+    document.querySelector('#modal_button').onclick = function() {
+        modal_switch();
+    }
+}
