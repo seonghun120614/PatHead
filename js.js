d277123c13d8ba7dc36103a7e35ade487aa84874
seonghun120614 <seonghun120614@gmail.com>
2021-01-31 06:22:04 +0000

    creating db

--- js.js
+++ js.js
@@ -1,15 +1,13 @@
 'use strict'
 
 class Modal {
-
-    constructor(content, status) {
-        this.content = content;
+    constructor(status) {
         this.status = false;
     }
+
     static switch() {
         if (this.status === false) {
             this.status = true;
-            document.getElementById('modal_content').textContent = 'hi';
             document.getElementById('modal').style.display = 'block';
         } else {
             this.status = false;
@@ -21,6 +19,25 @@ class Modal {
 
 
 window.onload = function() {
-    for (let i=1; i<4; i++) {document.querySelector('.item:nth-child(' + i + ')').onclick = function(){Modal.switch();}}
+    let data;
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'Data.json');
+    alert(ourRequest.responseText);
+
+
+    for (let i=1; i<4; i++) {document.querySelector('.item:nth-child(' + i + ')').onclick = function(){
+        Modal.switch();
+        switch (i) {
+            case 1 :
+                readTextFile("Data/Study/index.txt");
+                break;
+            case 2 :
+                readTextFile("Data/Rest/index.txt");
+                break;
+            case 3 :
+                readTextFile("Data/Inf/index.txt");
+                break;
+        }
+    }}
     document.querySelector('#modal_button').onclick = function() {Modal.switch();}
 }
