6b9adf796f3f4daf132121043e0891d03a32768f
seonghun120614 <seonghun120614@gmail.com>
2021-02-07 07:03:09 +0000

    mongodb

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 var app = express();
-const multer = require('multer');
-var _storage = multer.diskStorage({
-    destination:function(req, file, cb){
-        cb(null, 'data/')
-    },
-    filename : function(req, file, cb){
-        cb(null, file.originalname)
-    }
-});
-var upload = multer({storage : _storage});
 
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
+app.locals.pretty = true;
 
 app.get('/', function(req, res){
     res.render('main', {title:"다양한 블로그 PAT Head", description :"여기는 메인 화면입니다."});
 });
-
-app.get('/write', function(req, res) {
-    
-})
 
 app.listen(3000, () => {
     console.log('Server open port : 3000');
