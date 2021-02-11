779deccd84151d404e17215b7e5a7d3633987c29
seonghun120614 <seonghun120614@gmail.com>
2021-02-11 05:18:00 +0000

    1

--- index.js
+++ index.js
@@ -4,117 +4,182 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const mongoose = require('mongoose');
-var router = require('./routes')(app, Book);
 
+/*
+const connect = async () =>{
+    if(process.env.NODE_ENV !== 'production') mongoose.set('debug', true);
 
-module.exports = () =>{
-    const connect = async () =>{
-        if(process.env.NODE_ENV !== 'production') mongoose.set('debug', true);
-
-        
-        try {
-            await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phdb.wisly.mongodb.net/phdb?retryWrites=true&w=majority`, {
-                dbName: 'nodejs',
-                useNewUrlParser: true,
-                useFindAndModify: false,
-                useCreateIndex: true
-            });
-            console.log('The server is conneted');
-        } catch(error){
-            console.error(error);
-        }
-    };
     
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phdb.wisly.mongodb.net/phdb?retryWrites=true&w=majority`, {
+            dbName: 'nodejs',
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+        console.log('The server is conneted');
+    } catch(error){
+        console.error(error);
+    }
+};
+
+connect();
+
+mongoose.connection.on('error', (error)=>{
+    console.error('몽고디비 연결 에러', error);
+});
+
+mongoose.connection.on('disconnected', ()=>{
+    console.error('몽고디비 연결 끊김, 연결 재시도');
     connect();
-
-    mongoose.connection.on('error', (error)=>{
-        console.error('몽고디비 연결 에러', error);
-    });
-
-    mongoose.connection.on('disconnected', ()=>{
-        console.error('몽고디비 연결 끊김, 연결 재시도');
-        connect();
-    });
-}
-
-var Book = require('./models/book');
+});
+*/
 
 
 app.set('view engine', 'pug');
+
 app.set('views', './views');
+
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({extended:true}));
+
 app.use(express.static('public'));
+
 app.locals.pretty = true;
 
 
-
+/*메인화면 오류 x*/
 app.get('/', function(req, res){
+
     res.render('main', {title:"다양한 블로그 PAT Head", description :"여기는 메인 화면입니다."});
+
 });
 
+
+/*글쓰기*/
 app.get('/write', function(req, res){
-    res.render('main', {title:'Title.', description:"Cont."})
+
+    res.render('main', {title:'Title.', description:"Cont."});
+
 });
+
+/*글쓰기 후 전송*/
 app.post('/write', function(req, res){
+
     if (entry){
+
         let title = req.body.title;
+
         let description = req.body.description;
+
         let url = `menu/${req.params.topic}/${req.params.field}/${req.params.index}/${req.params.id}/`;
+
         fs.writeFile(url+title, description, function(err){
-            if(err){
-                res.status(500).send('Interval server error');
-            }
+
+            errChk(err);
+
             res.redirect(url+title);
+
         });
+
     } else {
+
         res.send('허가되지 않았습니다.');
+
     }
+
 });
-app.get('/menu/:topic/:field/:id',function(req, res){
-    let path = 'menu/'+req.params.topic+'/'+req.params.field+'/'+req.params.id;
-    if (req.params.topic == "study"){
-        fs.readdir(path + '/', function(err, files){
-            if(err){
-                console.log(err);
-                res.send('err');
-            }
-            let title = req.params.id;
-            let description = files;
-            res.render('main', {title:title, description:description, topic:req.params.topic});
-        });
-    } else {
-        fs.readFile(path, {encoding:'utf-8'}, function(err, data){
-            if(err){
-                console.log(err);
-                res.send('err');
-            }
-            let title = req.params.id;
-            let description = data;
-            res.render(title, description);
-        })
-    }
-})
+
+
+/* 메뉴화면 */
 app.get('/menu/:topic/:field', function(req, res){
+    let topic = req.params.topic;
     let title = req.params.field;
-    fs.readdir('menu/'+req.params.topic+'/'+title+'/', function(err, files){
-        if(err){
-            console.log(err);
-            res.send('err');
+    
+
+    fs.readdir(path+'/', function(err, files){
+        if (err){
+            errChk(err);
+            res.send(err);
         }
         let description = files;
-        res.render('main', {title:title, description:description, topic:req.params.topic});
+        res.render('main', {title:title, description:description, path:path});
+    });
+
+});
+
+/* Study의 세부 과목들 구성 */
+app.get('/menu/study/:field/:subject', (req, res)=>{
+    let field = req.params.field;
+    let subject = req.params.subject;
+    
+    let path = `/menu/study/${field}/${subject}/`;
+    fs.readdir(path, (err, files)=>{
+        if (err){
+            errChk(err);
+            res.send(err);
+        }
+        let description = files;
+        res.render('main',{title:subject, description:description});
     });
 });
 
+/* Rest의 게시글 보여주기 */
+app.get('/menu/rest/:field', (req, res)=>{
+    let field = req.params.field;
+    let subject = req.params.subject;
+
+    fs.readFile(url, (err, data)=>{
+        if(err){
+            errChk(err);
+            res.send(err);
+        }
+        let description = data;
+        res.render('main',{title:subject, description:description});
+    });
+});
+
+/* Study의 게시글 보여주기 */
+app.get('/menu/study/:field/:subject/:id', (req, res)=>{
+    let topic = req.params.topic;
+    let field = req.params.field;
+    let subject = req.params.subject;
+    let id = req.params.id;
+
+    let path = `/menu/study/${field}/${subject}/${id}`
+    fs.readFile(path, "utf8", (err, data)=>{
+
+        errChk(err);
+
+        res.render('main', {title:id, description:data});
+    });
+    
+});
+
+/* 드라이브 구성 */
 app.get('/drive', function(req, res){
+
     res.render('main', {title:'drive', description: 'my chest'});
+
 });
 
+/* 나의 포트폴리오 보여주기 */
 app.get('/inf', function(req, res){
+
     res.render('main', {title:'MY Information', description:'blur blurblur'});
+
 });
 
+/* 서버를 3000포트로 열고 callback함수 실행 */
 app.listen(3000, () => {
+
     console.log('Server open port : 3000');
+    
 });
+
+
+
+function errChk(err){
+    console.log(err);
+}
