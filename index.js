3b8feddf062031a276648b22d4721efd1d0fefc2
seonghun120614 <lnmin1206@gnu.ac.kr>
2021-02-12 07:41:41 +0000

    i find free gitpod!!!

--- index.js
+++ index.js
@@ -41,89 +41,10 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(express.static('fonts'));
 app.locals.pretty = true;
 
 
-/*메인화면 오류 x*/
-app.get('/', function(req, res){
-
-    res.render('main', {title:"다양한 블로그 PAT Head", description :"여기는 메인 화면입니다."});
-
-});
-
-
-/*글쓰기*/
-app.get('/write', function(req, res){
-
-    res.render('main', {title:'Title.', description:"Cont."});
-
-});
-
-/*글쓰기 후 전송*/
-app.post('/write', function(req, res){
-
-    if (entry){
-
-        let title = req.body.title;
-
-        let description = req.body.description;
-
-        let url = `menu/${req.params.topic}/${req.params.field}/${req.params.index}/${req.params.id}/`;
-
-        fs.writeFile(url+title, description, function(err){
-
-            errChk(err);
-
-            res.redirect(url+title);
-
-        });
-
-    } else {
-
-        res.send('허가되지 않았습니다.');
-
-    }
-
-});
-
-
-/* 메뉴화면 */
-app.get('/menu/:topic/:field', function(req, res){
-    let topic = req.params.topic;
-    let title = req.params.field;
-    let path = `menu/${topic}/${title}/`
-
-    fs.readdir(path, function(err, files){
-        if (err){console.log(err);res.send(err);}
-        let description = files;
-        res.render('main', {title:title, description:description});
-    });
-
-});
-
-/* Study의 세부 과목들 보여주기 */
-app.get('/menu/study/:field/:subject', (req, res)=>{
-    let field = req.params.field;
-    let subject = req.params.subject;
-    
-    let path = `menu/study/${field}/${subject}`;
-    fs.readdir(path, (err, files)=>{
-        if (err){console.log(err); res.send(err);}
-        let description = files;
-        res.render('main',{title:subject, description:description});
-    });
-});
-
-/* Rest의 세부 항목들 보여주기 */
-app.get('/menu/rest/:field', (req, res)=>{
-    let field = req.params.field;
-    let path = `menu/rest/${field}`
-    fs.readFile(url, (err, data)=>{
-        if(err){console.log(err);res.send(err);}
-        let description = data;
-        res.render('main',{title:subject, description:description});
-    });
-});
 
 /* Study의 게시글 보여주기 */
 app.get('/menu/study/:field/:subject/:id', (req, res)=>{
@@ -131,31 +52,35 @@ app.get('/menu/study/:field/:subject/:id', (req, res)=>{
     let field = req.params.field;
     let subject = req.params.subject;
     let id = req.params.id;
-
-    let path = `menu/study/${field}/${subject}/${id}`
+    let path = `menu/study/${field}/${subject}/${id}`;
     fs.readFile(path, "utf8", (err, data)=>{
         if (err){console.log(err);res.send(err);}
-
         fs.stat(path, (err, stats)=>{
             if (err){console.log(err);res.send(err);}
-
             let mtime = stats.mtime;
-            
+        
             let year = mtime.getFullYear();
             let month = mtime.getMonth() + 1;
             let date = mtime.getDate();
-
-            let hour = mtime.getHours();
-            let min = mtime.getMinutes();
-            let sec = mtime.getSeconds();
-
             let Day = {0:"Sun", 1:"Mon", 2:"Tue", 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sun'};
             let day = Day[mtime.getDay()];
 
-            let time = `${year}-${month}-${date} ${hour}:${min}:${sec} (${day})`;
+            let time = `${year}-${month}-${date} (${day})`;
 
             res.render('main', {title:id, description:data, time:time});
         });
+    });
+});
+
+/* Study의 세부 과목들 보여주기 */
+app.get('/menu/study/:field/:subject', (req, res)=>{
+    let field = req.params.field;
+    let subject = req.params.subject;
+    let path = `menu/study/${field}/${subject}`;
+    fs.readdir(path, (err, files)=>{
+        if (err){console.log(err); res.send(err);}
+        let description = files;
+        res.render('main',{title:subject, description:description});
     });
 });
 
@@ -165,31 +90,86 @@ app.get('/menu/rest/:field/:id', (req,res)=>{
     let id = req.params.id;
     let path = `menu/rest/${field}/${id}`;
 
-    fs.readFile(path, (err, data)=>{
+    fs.readFile(path, "utf8", (err, data)=>{
         if(err){console.log(err); res.send(err);}
 
         fs.stat(path, (err, stats)=>{
             if (err){console.log(err);res.send(err);}
-
             let mtime = stats.mtime;
-            res.render('main', {title:id, description:data, time:mtime});
+        
+            let year = mtime.getFullYear();
+            let month = mtime.getMonth() + 1;
+            let date = mtime.getDate();
+            let Day = {0:"Sun", 1:"Mon", 2:"Tue", 3:'Wed', 4:'Thu', 5:'Fri', 6:'Sun'};
+            let day = Day[mtime.getDay()];
+
+            let time = `${year}-${month}-${date} (${day})`;
+
+            res.render('main', {title:id, description:data, time:time});
         });
     });
 });
 
+/* Rest의 세부항목들 보여주기 */
+app.get('/menu/rest/:field', (req, res)=>{
+    let field = req.params.field;
+    let path = `menu/rest/${field}`;
+    fs.readdir(path, (err, files)=>{
+        if(err){console.log(err);res.send(err);}
+        res.render('main',{title:field, description:files});
+    });
+});
+
+/* 메뉴화면 */
+app.get('/menu/:topic/:field', (req, res)=>{
+    let topic = req.params.topic;
+    let title = req.params.field;
+    let path = `menu/${topic}/${title}/`
+    fs.readdir(path, function(err, files){
+        if (err){console.log(err);res.send(err);}
+        let description = files;
+        res.render('main', {title:title, description:description});
+    });
+});
+
 /* 드라이브 구성 */
-app.get('/drive', function(req, res){
+app.get('/drive', (req, res)=>{
 
     res.render('main', {title:'drive', description: 'my chest'});
 
 });
 
 /* 나의 포트폴리오 보여주기 */
-app.get('/inf', function(req, res){
+app.get('/inf', (req, res)=>{
 
     res.render('main', {title:'MY Information', description:'blur blurblur'});
 
 });
+
+/*글쓰기*/
+app.get('/write', (req, res)=>{
+    res.render('main', {title:'Title.', description:"Cont."});
+});
+
+/*글쓰기 후 전송*/
+app.post('/write', (req, res)=>{
+    let title = req.body.title;
+    let description = req.body.description;
+    console.log(title, description);
+    let url = `menu/${req.body.topic}/${req.body.field}/${req.body.subject}/${title}`;
+    fs.writeFile(url, description, function(err){
+        if (err){console.log(err);res.send(err);}
+        res.render('main',{title:title, description:description});
+    });
+});
+
+/* 로그인 */
+app.get('/login', (req, res) =>{
+    res.render('main', {title:'login', description:'description'});
+});
+
+/*메인화면*/
+app.get('/', (req, res)=>{res.render('main', {title:"다양한 블로그 PAT Head", description :"여기는 메인 화면입니다."});});
 
 /* 서버를 3000포트로 열고 callback함수 실행 */
 app.listen(3000, () => {
--- public/javascript.js
+++ public/javascript.js
@@ -1,14 +1,94 @@
 'use strict'
 
+const study = {"ai":["deep learning", 'reinforcement learning', 'supervised learning', 'unsupervised learning'],
+"computer":['c','css','html','java','javascript','python'],
+"data technology":['data mining','data processing'],
+"math":['calculus','linear algebra','number theorem','set theorem'],
+"statistics":["descriptive statistics", "inferential statistics"]};
+const rest = ["free","game","life","trip"];
+
+
 window.onload = function(){
-    window.setInterval(showTime, 1000);
 
     function showTime() {
         let date = new Date();
         document.getElementById('time').innerHTML = date;
     }
+    
+    function ChildRemoveAll(id){
+        let tag = document.getElementById(id);
+        while(tag.childElementCount>1){
+            tag.removeChild(tag.lastChild);
+        }
+    }
 
-    function formOption(){
-        
+    function AddFormOption(select_id, option_list){
+        for (let i in option_list) {
+            let option = document.createElement('option');
+            let option_attr = document.createAttribute('value');
+            option_attr.value = option_list[i];
+            option.setAttributeNode(option_attr);
+            option.innerText = option_list[i];
+            document.getElementById(select_id).appendChild(option);
+        }
+    }
+
+    function showPath(path) {
+        document.getElementById('path').innerText = str;
+    }
+
+    try{
+        document.getElementById('topic').onchange = ()=>{
+            let topic = document.getElementById('topic').value;
+            let title = document.getElementById('title').value;
+            let path = topic + '/';
+
+            if(topic!=""){
+
+                document.getElementById('field').removeAttribute('class');
+                ChildRemoveAll('field');
+
+                if (topic==="study"){
+                    AddFormOption('field', Object.keys(study));
+                } else if(topic==="rest"){
+                    AddFormOption('field', rest);
+                }
+
+            } else {
+
+                document.getElementById('field').setAttribute('class', 'hidden');
+                document.getElementById('subject').setAttribute('class', 'hidden');
+            }
+
+            document.getElementById('path').value = path;
+        };
+
+        document.getElementById('field').onchange = ()=>{
+            let field = document.getElementById('field').value;
+            let title = document.getElementById('title').value;
+            let path = document.getElementById('topic').value + '/' + field + '/';
+            
+            if (field in study) {
+
+                document.getElementById('subject').removeAttribute('class');
+                ChildRemoveAll('subject');
+                AddFormOption('subject', study[field]);
+                
+            } else {
+                document.getElementById('subject').setAttribute('class', 'hidden');
+            }
+            document.getElementById('path').value = path;
+        };
+
+        document.getElementById('subject').onchange = ()=>{
+            let title = document.getElementById('title').value;
+            let path = document.getElementById('topic').value + '/' + document.getElementById('field').value+ '/' +document.getElementById('subject').value + '/';
+            document.getElementById('path').value = path;
+        }
+    } catch {
+        console.log("Error");
+    } finally {
+        showTime();
+        window.setInterval(showTime, 1000);
     }
 }
